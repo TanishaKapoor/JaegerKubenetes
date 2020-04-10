2e396a5d423d427277d7ce595f6c7ce60c63e325
Tanisha Kapoor <tanisha.kapoor@nagarro.com>
2020-04-10 22:08:37 +0530

    JaegerAssignment

---
+++ UserService/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("user")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        List<User> _userdb = new List<User>();
+        UserContext context;
+        public ValuesController()
+        {
+           
+        }
+        // GET api/values
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            _userdb = context.GetAllUsers();
+            return Ok(_userdb);
+        }
+
+        // GET user/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            context = HttpContext.RequestServices.GetService(typeof(UserContext)) as UserContext;
+            _userdb = context.GetAllUsers();
+            var user = _userdb.Where(c => c.id == id).Select(c=>new {c.age,c.name,c.email}).FirstOrDefault();
+            return Ok(user);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ UserService/UserContext.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService
+{
+    public class UserContext
+    {
+        public string ConnectionString { get; set; }
+
+        public UserContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+        public List<User> GetAllUsers()
+        {
+            List<User> list = new List<User>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from User", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new User()
+                        {
+                            id = Convert.ToInt32(reader["id"]),
+                            name = reader["name"].ToString(),
+                            email = reader["email"].ToString(),
+                            age = Convert.ToInt32(reader["age"])
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
